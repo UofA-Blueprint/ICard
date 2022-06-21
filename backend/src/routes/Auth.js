e1e36658ec3e20c2f81445e7ccdec71f4c181d28
ArmaanKatyal <armaankatyal3@gmail.com>
2022-06-21 01:25:12 -0600

    added route for register controller

--- backend/src/routes/Auth.js
+++ backend/src/routes/Auth.js
@@ -4,5 +4,6 @@ const authController = require('../controllers/Auth')
 const router = express.Router()
 
 router.post('/login', authController.login) // login
+router.post('/register', authController.register) // register
 
 module.exports = router

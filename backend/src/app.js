9795f9167b69f92bb266e6bb185e4357506a9e36
ArmaanKatyal <armaankatyal3@gmail.com>
2022-06-21 14:27:16 -0600

    fixed db conn issue during testing

--- backend/src/app.js
+++ backend/src/app.js
@@ -9,25 +9,35 @@ const authRouter = require('./routes/Auth')
 const app = express()
 dotenv.config()
 
-mongoose.connect(process.env.MONGO_URI, {
-    // connect to mongoDB
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-})
+if (process.env.NODE_ENV === 'test') {
+    // if we are in test mode, we use the test database
+    mongoose.connect(process.env.MONGO_URI_TEST, {
+        // connect to mongoDB
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    })
+} else {
+    mongoose.connect(process.env.MONGO_URI, {
+        // if we are in production mode, we use the production database
+        // connect to mongoDB
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    })
+}
 
 var db = mongoose.connection // get the connection
 db.on('error', console.error.bind(console, 'connection error:'))
 
-app.use(cors())
+app.use(cors()) // enable CORS
 
 app.use(express.json()) // for parsing application/json
 
-app.get('/', (req, res) => {
+app.get('/test_db_conn', (req, res) => {
     // test db connection
     res.json({ status: db.readyState, database: db.name })
 })
 
-app.use('/api/auth', authRouter)
-app.use('/api/students', studentsRouter)
+app.use('/api/auth', authRouter) // auth routes
+app.use('/api/students', studentsRouter) // student routes
 
 module.exports = app // export the app for testing

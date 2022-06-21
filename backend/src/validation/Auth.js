b06eee1c4b8367617db30ca1bda4e2058a8f95b7
ArmaanKatyal <armaankatyal3@gmail.com>
2022-06-21 01:25:29 -0600

    added validation for register req.body

--- backend/src/validation/Auth.js
+++ backend/src/validation/Auth.js
@@ -1,14 +1,26 @@
 const Joi = require('joi')
 
-const validateLogin = (data) => {
-    // valiidate the login request body
-    const schema = Joi.object({
-        // create a schema
-        email: Joi.string().min(6).max(255).required().email(),
-        password: Joi.string().min(5).max(1024).required(),
-    })
+module.exports = {
+    validateLogin: (data) => {
+        // valiidate the login request body
+        const schema = Joi.object({
+            // create a schema
+            email: Joi.string().min(6).max(255).required().email(),
+            password: Joi.string().min(5).max(1024).required(),
+        })
 
-    return schema.validate(data) // validate the data
+        return schema.validate(data) // validate the data
+    },
+    validateRegister: (data) => {
+        // validate the register request body
+        const schema = Joi.object({
+            // create a schema
+            name: Joi.string().min(3).max(255).required(),
+            email: Joi.string().min(6).max(255).required().email(),
+            password: Joi.string().min(5).max(1024).required(),
+            icard_number: Joi.string().min(5).max(1024).required(),
+        })
+
+        return schema.validate(data) // validate the data
+    }
 }
-
-module.exports = validateLogin

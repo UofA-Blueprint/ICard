e0b4b99dcc42e0b181f7fe2089806a76d5d77667
ArmaanKatyal <armaankatyal3@gmail.com>
2022-06-21 14:28:59 -0600

    added status code and fixed update response issue

--- backend/src/controllers/Students.js
+++ backend/src/controllers/Students.js
@@ -22,7 +22,7 @@ module.exports = {
         const student = new Student(req.body) // create a new student
         try {
             const newStudent = await Student.create(student) // create the student
-            res.json(newStudent) // return the student
+            res.status(201).json(newStudent) // return the student
         } catch (err) {
             res.status(500).json({ message: err }) // if error, send 500 status
         }
@@ -37,10 +37,8 @@ module.exports = {
 
         try {
             // Update the student
-            const updatedStudent = await Student.findByIdAndUpdate(
-                req.params.studentId,
-                req.body)
-            res.json(updatedStudent) // return the student
+            await Student.findByIdAndUpdate(req.params.studentId, req.body)
+            res.status(200).json({ message: 'Student Updated' }) // return the student
         } catch (err) {
             res.status(500).json({ message: err }) // if error, send 500 status
         }

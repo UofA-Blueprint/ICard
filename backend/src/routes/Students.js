c7c4cbf69be54419ada65368afd879f65d3dd4ae
ArmaanKatyal <armaankatyal3@gmail.com>
2022-06-22 00:37:17 -0600

    added the new middleware

--- backend/src/routes/Students.js
+++ backend/src/routes/Students.js
@@ -1,13 +1,13 @@
 const express = require('express')
 const studentController = require('../controllers/Students')
-const verifyToken = require('../authentication/verifyToken')
+const { verifyApiKey } = require('../authentication/verifyToken')
 
 const router = express.Router()
 
-// verifyToken is a middleware that checks if the user is authenticated
-router.get('/:studentId', verifyToken, studentController.getStudent) // get a student by id
-router.post('/', verifyToken, studentController.create) // create a new student
-router.put('/:studentId', verifyToken, studentController.update) // update a student
-router.delete('/:studentId', verifyToken, studentController.delete) // delete a student
+// verifyToken is a middleware that checks if the user is authenticated and has the correct api-key
+router.get('/:studentId', verifyApiKey, studentController.getStudent) // get a student by id
+router.post('/', verifyApiKey, studentController.create) // create a new student
+router.put('/:studentId', verifyApiKey, studentController.update) // update a student
+router.delete('/:studentId', verifyApiKey, studentController.delete) // delete a student
 
 module.exports = router

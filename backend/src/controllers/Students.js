699bcb14a97955ca3729e8840180c11d1f422602
ArmaanKatyal <armaankatyal3@gmail.com>
2022-06-26 01:26:27 -0600

    added new controller and removed unnecessary comments

--- backend/src/controllers/Students.js
+++ backend/src/controllers/Students.js
@@ -2,54 +2,60 @@ const Student = require('../models/Student')
 const validateStudent = require('../validation/Student')
 
 module.exports = {
+    getAll: async (req, res) => {
+        try {
+            const students = await Student.find()
+            res.status(200).json(students)
+        } catch (err) {
+            res.status(500).json({ message: err })
+        }
+    },
     getStudent: async (req, res) => {
         try {
-            const student = await Student.findById(req.params.studentId) // get the student by id
-            res.json(student) // send the student
+            const student = await Student.findById(req.params.studentId)
+            res.json(student)
         } catch (err) {
-            res.status(500).json({ message: err }) // if error, send 500 status
+            res.status(500).json({ message: err })
         }
     },
     create: async (req, res) => {
         const { error } = validateStudent(req.body) // validate the data
         if (error)
-            return res.status(400).json({ message: error.details[0].message }) // if error, return 400 with error message
+            return res.status(400).json({ message: error.details[0].message })
         // Check if student already exists
         const checkStudent = await Student.findOne({ email: req.body.email })
         if (checkStudent)
             return res.status(400).json({ message: 'Student already exists' }) // if student already exists, return 400 with error message
 
-        const student = new Student(req.body) // create a new student
+        const student = new Student(req.body)
         try {
-            const newStudent = await Student.create(student) // create the student
-            res.status(201).json(newStudent) // return the student
+            const newStudent = await Student.create(student)
+            res.status(201).json(newStudent)
         } catch (err) {
-            res.status(500).json({ message: err }) // if error, send 500 status
+            res.status(500).json({ message: err })
         }
     },
     update: async (req, res) => {
-        // Check if student exists
-        const checkStudent = await Student.findOne({
+        const checkStudent = await Student.findOne({ // Check if student exists
             _id: req.params.studentId,
         })
         if (!checkStudent)
-            return res.status(400).json({ message: 'Student does not exist' }) // if student does not exist, return 400 with error message
+            return res.status(400).json({ message: 'Student does not exist' })
 
         try {
             // Update the student
             await Student.findByIdAndUpdate(req.params.studentId, req.body)
-            res.status(200).json({ message: 'Student Updated' }) // return the student
+            res.status(200).json({ message: 'Student Updated' })
         } catch (err) {
-            res.status(500).json({ message: err }) // if error, send 500 status
+            res.status(500).json({ message: err })
         }
     },
     delete: async (req, res) => {
-        // Check if student exists
-        const checkStudent = await Student.findOne({
+        const checkStudent = await Student.findOne({ // Check if student exists
             _id: req.params.studentId,
         })
         if (!checkStudent)
-            return res.status(400).json({ message: 'Student does not exist' }) // if student does not exist, return 400 with error message
+            return res.status(400).json({ message: 'Student does not exist' })
 
         try {
             // Delete the student
@@ -58,7 +64,7 @@ module.exports = {
             )
             res.json(deletedStudent)
         } catch (err) {
-            res.status(500).json({ message: err }) // if error, send 500 status
+            res.status(500).json({ message: err })
         }
     },
 }

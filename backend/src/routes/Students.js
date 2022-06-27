c47908c975d7b5c74a414e130ca767eb0f9b55c3
ArmaanKatyal <armaankatyal3@gmail.com>
2022-06-27 08:12:38 -0600

    added a new route

--- backend/src/routes/Students.js
+++ backend/src/routes/Students.js
@@ -7,6 +7,7 @@ const router = express.Router()
 // verifyToken is a middleware that checks if the user is authenticated and has the correct api-key
 router.get('/all', verifyApiKey, studentController.getAll) // get all students
 router.get('/:studentId', verifyApiKey, studentController.getStudent) // get a student by id
+router.get('/icard/:icard_number', verifyApiKey, studentController.getStudentByIcard) // get a student by icard number
 router.post('/', verifyApiKey, studentController.create) // create a new student
 router.put('/:studentId', verifyApiKey, studentController.update) // update a student
 router.delete('/:studentId', verifyApiKey, studentController.delete) // delete a student

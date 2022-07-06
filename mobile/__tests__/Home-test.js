51053e50c19899bbdd41bb101983153069fed3d8
Duc Ho <hotienduc05012002@gmail.com>
2022-07-06 22:59:39 +0700

    Additional adjust for logging after test

--- mobile/__tests__/Home-test.js
+++ mobile/__tests__/Home-test.js
@@ -2,7 +2,7 @@ import React from 'react';
 import renderer from 'react-test-renderer';
 import HomeView from '../src/views/HomeView';
 
-test('renders correctly', () => {
+test('renders correctly', async () => {
   const tree = renderer.create(<HomeView />).toJSON();
   expect(tree).toMatchSnapshot();
 });

ae25843906158ced38d01b6ee4c15b515ea2d958
Duc Ho <hotienduc05012002@gmail.com>
2022-07-28 20:17:23 +0700

    Added login functionality with private google client id

--- mobile/src/views/RegistrationView.jsx
+++ mobile/src/views/RegistrationView.jsx
@@ -1,19 +1,64 @@
-import React from 'react';
-import {View, Text, StyleSheet, TouchableOpacity} from 'react-native';
+import React, {useState} from 'react';
+import {View, Text, StyleSheet, TouchableOpacity, Image} from 'react-native';
 import {FontAwesome5} from '@expo/vector-icons';
 
 import Header from '../components/shared/Header';
 import {colors, globalStyleSheet} from '../utilites/Theme';
 
+import * as WebBrowser from 'expo-web-browser';
+import * as Google from 'expo-auth-session/providers/google';
+
+WebBrowser.maybeCompleteAuthSession();
+
 const RegistrationView = () => {
+  const [accessToken, setAccessToken] = useState();
+  const [userInfo, setUserInfo] = useState();
+
+  const [request, response, promptAsync] = Google.useAuthRequest({
+    expoClientId:
+      '274455879293-j240h2dtj0n2n90f8l8hsdngnct4iqk5.apps.googleusercontent.com',
+  });
+
+  async function getUserData() {
+    let userData = await fetch(
+      'https://www.googleapis.com/oauth2/v2/userinfo',
+      {
+        headers: {Authorization: 'Bearer ' + accessToken},
+      },
+    );
+    userData.json().then(data => setUserInfo(data));
+  }
+
+  React.useEffect(() => {
+    if (response?.type === 'success') {
+      const {authentication} = response; // storing our authentication data (JSON) into this authentication variable
+      setAccessToken(authentication.accessToken);
+      getUserData();
+    }
+  }, [response]);
+
   return (
     <View style={globalStyleSheet.container}>
       <Header />
       <View style={[styles.bodyContainer]}>
-        <TouchableOpacity onPress={() => {}} style={styles.signInButton}>
-          <FontAwesome5 name="google" size={16} color={colors.primary} />
-          <Text style={styles.promptMessage}>Sign In with Google</Text>
-        </TouchableOpacity>
+        {userInfo ? (
+          <View style={styles.loggedIn}>
+            <Text>Logged In</Text>
+            <Image
+              source={{uri: userInfo['picture']}}
+              style={styles.avatar}></Image>
+          </View>
+        ) : (
+          <TouchableOpacity
+            disabled={!request}
+            onPress={() => {
+              promptAsync();
+            }}
+            style={styles.signInButton}>
+            <FontAwesome5 name="google" size={16} color={colors.primary} />
+            <Text style={styles.promptMessage}>Sign In with Google</Text>
+          </TouchableOpacity>
+        )}
       </View>
     </View>
   );
@@ -37,6 +82,15 @@ const styles = StyleSheet.create({
   },
   promptMessage: {
     color: colors.primary,
+  },
+  loggedIn: {
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  avatar: {
+    width: 50,
+    height: 50,
+    borderRadius: 25,
   },
 });
 

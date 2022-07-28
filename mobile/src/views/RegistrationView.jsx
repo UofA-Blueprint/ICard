0e431a4986445a95e40b3153ba3bd20b0c373520
Duc Ho <hotienduc05012002@gmail.com>
2022-07-28 14:17:52 +0700

    Added Google Sign In button

--- mobile/src/views/RegistrationView.jsx
+++ mobile/src/views/RegistrationView.jsx
@@ -1,15 +1,19 @@
 import React from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import {View, Text, StyleSheet, TouchableOpacity} from 'react-native';
+import {FontAwesome5} from '@expo/vector-icons';
 
 import Header from '../components/shared/Header';
-import {globalStyleSheet} from '../utilites/Theme';
+import {colors, globalStyleSheet} from '../utilites/Theme';
 
 const RegistrationView = () => {
   return (
     <View style={globalStyleSheet.container}>
       <Header />
       <View style={[styles.bodyContainer]}>
-        <Text>My ICard</Text>
+        <TouchableOpacity onPress={() => {}} style={styles.signInButton}>
+          <FontAwesome5 name="google" size={16} color={colors.primary} />
+          <Text style={styles.promptMessage}>Sign In with Google</Text>
+        </TouchableOpacity>
       </View>
     </View>
   );
@@ -17,7 +21,22 @@ const RegistrationView = () => {
 
 const styles = StyleSheet.create({
   bodyContainer: {
+    ...globalStyleSheet.fullScreen,
     justifyContent: 'center',
+  },
+  signInButton: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    alignItems: 'center',
+    width: '60%',
+    paddingHorizontal: 24,
+    paddingVertical: 8,
+    borderWidth: 1,
+    borderRadius: 8,
+    borderColor: colors.primary,
+  },
+  promptMessage: {
+    color: colors.primary,
   },
 });
 

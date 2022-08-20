02f4ce8695faca307ab9cc992d2f495e6c20b1ef
Armaan Katyal <armaankatyal3@gmail.com>
2022-08-20 10:47:53 -0600

    Feature/112: Google OAuth integration for backend and adminbro (#18)
    * added support for swagger api docs
    
    * updates
    
    * updates
    
    * readme for swagger api docs
    
    * updates
    
    * controller for login route
    
    * updated the Student model for google auth
    
    * added the new route
    
    * added new middle ware to verify google token
    
    * admin-bro support
    
    * Update package-lock.json
    
    * Update package.json
    
    Co-authored-by: Armaan <armaanakatyal3@gmail.com>

--- backend/src/routes/Auth.js
+++ backend/src/routes/Auth.js
@@ -1,9 +1,9 @@
 const express = require('express')
 const authController = require('../controllers/Auth')
+const { checkAuthenticated } = require('../services/verifyToken')
 
 const router = express.Router()
 
-router.post('/login', authController.login) // login
-router.post('/register', authController.register) // register
+router.post('/login', checkAuthenticated, authController.login) // login
 
 module.exports = router

c17ac6d4b85a1f79238224d79582031d6f6a4244
Gurveer Sohal <gurveersohal433@gmail.com>
2022-08-25 08:18:28 -0600

    styled to match the mockup

--- mobile/src/components/shared/VendorCard.jsx
+++ mobile/src/components/shared/VendorCard.jsx
@@ -1,86 +1,173 @@
 import React from 'react';
-import {View, Text, StyleSheet, Image, Dimensions} from 'react-native';
+import { render } from 'react-dom';
+import {View, Text, StyleSheet, Image, Dimensions, Modal, Button} from 'react-native';
+import Pressable from 'react-native/Libraries/Components/Pressable/Pressable';
 import {colors, globalStyleSheet} from '../../utilites/Theme';
+import { useState } from 'react';
 
-const VendorCard = ({vendorName, location, description, vendorImage, cardDesc, discount}) => {
-  return (
-    <View style={styles.container}>
-    	<Image style={styles.logo} source={{uri: vendorImage}}></Image>
+const VendorCard = ({vendorName, location, description, vendorImage, cardDesc, discount, contact}) => {
+    const [popupOpen, setPopupOpen] = useState(false);
+    
+    return (
+        <View>
+            <Modal visible={popupOpen} transparent={true} animationType='fade'>
+                <View style={{
+                    flex: 1,
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                    backgroundColor: '#00000088'
+                }}>
+                    <View style={styles.modalView}>
+                        <View style={{
+                                display: 'flex',
+                                flexDirection: 'row',
+                                justifyContent: 'center',
+                                alignItems: 'center',
+                                paddingHorizontal: 10,
+                                paddingVertical: 5
+                        }}>
+                            <View style={{flexShrink: 2, paddingHorizontal: 10}}>
+                                <Text style={styles.title}>
+                                {vendorName}
+                                </Text>
+                            </View>
 
-      	<View style={{flexShrink: 2}}>
+                            <Image style={styles.logo} source={{uri: vendorImage}}></Image>
+                            
+                        </View>
 
-			<Text style={styles.title}>
-			{vendorName}
-			</Text>
+                        <Text style={{
+                            textAlign:'center',
+                            fontWeight: 'bold',
+                            fontSize: 12,
+                            width: 250,
+                            marginBottom: 10
+                        }}>
+                            {description}
+                        </Text>
 
-			<View style={{
-				display: 'flex',
-				flexDirection: 'row',
-				justifyContent: 'center',
-				alignItems: 'center'
-			}}>
-				<Text style={styles.discount}>
-					{discount}
-				</Text>
+                        <Text style={styles.text}>
+                            Location: {location}
+                        </Text>
 
-				<Text style={styles.cardDesc}>
-					{cardDesc}
-				</Text>
-			</View>
+                        <Text style={styles.text}>
+                            Contact: {contact}
+                        </Text>
 
+                        <Pressable
+                        style={{
+                            borderRadius: 20,
+                            padding: 5,
+                            marginVertical: 10,
+                            backgroundColor: colors.primary
+                        }}
+                        onPress={() => setPopupOpen(false)}
+                        >
+                            <Text style={{
+                                color: 'white'
+                            }}>Close</Text>
+                        </Pressable>
+                      </View>
+                </View>
+            </Modal>
+
+            <Pressable style={styles.container} onPress={() => setPopupOpen(true)}>
+                <Image style={styles.logo} source={{uri: vendorImage}}></Image>
+
+                <View style={{flexShrink: 2}}>
+
+                    <Text style={styles.title}>
+                    {vendorName}
+                    </Text>
+
+                    <View style={{
+                        display: 'flex',
+                        flexDirection: 'row',
+                        justifyContent: 'center',
+                        alignItems: 'center'
+                    }}>
+                        <Text style={styles.discount}>
+                            {discount}
+                        </Text>
+
+                        <Text style={styles.text}>
+                            {cardDesc}
+                        </Text>
+                    </View>
+
+                </View>
+            </Pressable>
         </View>
-    </View>
-  );
+    );
 };
 
 const win = Dimensions.get('window');
 
 const styles = StyleSheet.create({
-  title: {
-    color: colors.primary,
-    fontSize: 20,
-    fontWeight: 'bold',
-	paddingHorizontal: 5
-  },
-  discount: {
-    color: colors.black,
-    fontSize: 30,
-    fontWeight: 'bold',
-	paddingHorizontal: 5
-  },
-  cardDesc: {
-    color: colors.black,
-    fontSize: 10,
-	paddingHorizontal: 5,
-	flexShrink: 1
-  },
-  logo: {
-    flexBasis: 75,
-    flexShrink: 0,
-    aspectRatio: 1,
-    resizeMode: 'contain'
-  },
-  textContainer: {
-    flexShrink: 2,
-	paddingHorizontal: 5
-  },
-  container: {
-    display: 'flex',
-    flexDirection: 'row',
-    justifyContent: 'center',
-    alignItems: 'center',
-    paddingVertical: 10,
-    paddingHorizontal: 16,
-    borderWidth: 1,
-    borderColor: colors.primary,
-    borderRadius: 10,
-    marginVertical: 12,
-    marginHorizontal: 20,
-    width: 0.8*win.width
-  },
-  description: {
-    paddingTop: 10,
-  },
+    title: {
+        color: colors.primary,
+        fontSize: 20,
+        fontWeight: 'bold',
+        marginBottom: 10,
+        marginLeft: 10
+    },
+
+    discount: {
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        textAlign: 'center',
+        color: colors.black,
+        fontWeight: 'bold',
+        paddingHorizontal: 5,
+        fontSize: 20,
+        width: 100,
+        flexShrink: 1,
+    },
+
+    text: {
+        color: colors.black,
+        fontSize: 10,
+        paddingHorizontal: 5,
+        flexShrink: 1
+    },
+
+    logo: {
+        flexBasis: 75,
+        flexShrink: 0.5,
+        aspectRatio: 1.5/1,
+        resizeMode: 'contain'
+    },
+
+    container: {
+        display: 'flex',
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center',
+        paddingVertical: 10,
+        paddingHorizontal: 16,
+        backgroundColor: 'white',
+        borderWidth: 1,
+        borderColor: colors.primary,
+        borderRadius: 10,
+        marginVertical: 12,
+        marginHorizontal: 20,
+        width: 0.8*win.width
+    },
+
+    description: {
+        paddingTop: 10,
+    },
+
+    modalView: {
+        backgroundColor: 'white',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderColor: colors.primary,
+        borderRadius: 20,
+        borderWidth: 1
+    }
 });
 
 export default VendorCard;
--- mobile/src/views/VendorView.jsx
+++ mobile/src/views/VendorView.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
-import {StyleSheet, View, FlatList} from 'react-native';
+import {StyleSheet, View, FlatList, Text} from 'react-native';
 import Header from '../components/shared/Header';
 import VendorCard from '../components/shared/VendorCard';
 import {globalStyleSheet} from '../utilites/Theme';
 import vendorData from '../data/vendorMockData';
+import {colors} from '../utilites/Theme'
 
 const VendorView = () => {
   const renderItem = ({item}) => (
@@ -13,12 +14,26 @@ const VendorView = () => {
       cardDesc={item.cardDesc}
       discount={item.discount}
       vendorImage={item.image}
+      description={item.popupDesc}
+      contact={item.contact}
     />
   );
 
   return (
-    <View style={styles.container}>
+    <View style={[styles.container, {backgroundColor: '#D9FFDC44'}]}>
       <Header />
+
+      <Text style={{
+        color: colors.primary,
+        fontSize: 30,
+        fontWeight: 'bold',
+        marginVertical: 10,
+        marginHorizontal: 20,
+        alignSelf: 'flex-start'
+      }}>
+        Vendors
+      </Text>
+
       <FlatList
         data={vendorData}
         renderItem={renderItem}

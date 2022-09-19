c80ee1fd8ec330acdbb0d3d1aaba89f8b7199c26
armaankatyal <armaankatyal3@gmail.com>
2022-09-19 10:10:24 -0600

    added jwt key with the user response

--- backend/src/controllers/Auth.js
+++ backend/src/controllers/Auth.js
@@ -1,4 +1,5 @@
 const Student = require('../models/Student')
+var jwt = require('jsonwebtoken')
 require('dotenv').config()
 
 module.exports = {
@@ -9,7 +10,18 @@ module.exports = {
         const check_student_db = await Student.findOne({ email: user.email }) // check if the user exists in the database
 
         if (check_student_db) {
-            return res.status(200).json(check_student_db) // if the user exists, return the user
+            const studentWithKey = {
+                name: check_student_db.name,
+                email: check_student_db.email,
+                active_status: check_student_db.active_status,
+                isaf_status: check_student_db.isaf_status,
+                verify: check_student_db.verify,
+                picture: check_student_db.picture,
+                key: jwt.sign({
+                    email: check_student_db.email,
+                }, process.env.JWT_SECRET, { expiresIn: '30d' }),
+            }
+            return res.status(200).json(studentWithKey) // if the user exists, return the user
         } else {
             const new_student = new Student({
                 // if the user doesn't exist, create a new user
@@ -23,7 +35,20 @@ module.exports = {
 
             try {
                 const new_student_db = await Student.create(new_student)
-                return res.status(200).json(new_student_db)
+
+                const studentWithKey = {
+                    name: new_student_db.name,
+                    email: new_student_db.email,
+                    active_status: new_student_db.active_status,
+                    isaf_status: new_student_db.isaf_status,
+                    verify: new_student_db.verify,
+                    picture: new_student_db.picture,
+                    key: jwt.sign({
+                        email: new_student_db.email,
+                    }, process.env.JWT_SECRET, { expiresIn: '30d' }),
+                }
+
+                return res.status(200).json(studentWithKey) // return the new user with the jwt key valid for 30 days
             } catch (err) {
                 return res.status(500).json({ message: err })
             }

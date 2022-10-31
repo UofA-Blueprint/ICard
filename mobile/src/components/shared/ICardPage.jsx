09d7dcc677cc281b219c00ce31642b6c82bf2abd
Aaron <aaronboyd385@gmail.com>
2022-10-31 13:33:53 -0600

    styled the Icardpage

--- mobile/src/components/shared/ICardPage.jsx
+++ mobile/src/components/shared/ICardPage.jsx
@@ -1,309 +1,181 @@
-import React from 'react';
-import {View, ImageBackground, Image, StyleSheet, Text, TouchableOpacity} from 'react-native';
-import Header from '../shared/Header';
-import {MaterialCommunityIcons} from '@expo/vector-icons';
-
+import React, {useContext} from 'react';
+import {View, Image, StyleSheet, Text, TouchableOpacity, ImageBackground} from 'react-native';
+import FancyBox from './FancyBox';
+ 
 import {
   useFonts,
   Poppins_600SemiBold,
   Poppins_700Bold,
 } from '@expo-google-fonts/poppins';
-
-import {colors, globalStyleSheet} from '../../utilites/Theme';
-import FancyBox from './FancyBox';
-
+ 
+import {colors} from '../../utilites/Theme';
+ 
+ 
+ 
+ 
+ 
 const statusColors = {
-    active: colors.lightGreen,
-    inactive: colors.red,
-    stale: colors.darkGray,
-    pending: colors.yellow,
-    Unlinked: colors.darkGray
-  };
-
-
+  active: colors.primary,
+  inactive: colors.red,
+  stale: colors.darkGray,
+  pending: colors.yellow,
+  Unlinked: colors.lightGray,
+};
+ 
 const MyICardPage = (props) => {
-
-    let [fontsLoaded] = useFonts({
-        Poppins_600SemiBold,
-        Poppins_700Bold,
-      });
-      if (!fontsLoaded) {
-        return <Text>Loading...</Text>;
-      }
-
-    const {user} = props;
-
-    return (
-    <View style={styles.container}>
-        
-        
-        <Image
-        source={
-            require('../../../assets/squiggly-lines.png')
-        }
-        style={styles.linesTop}
-        />
-        <TouchableOpacity
-        style={styles.refreshButton}
-        onPress={() => {
-            console.log('Pressed!');
-        }}>
-        <MaterialCommunityIcons
-            name="refresh"
-            size={36}
-            color={colors.darkGray}
-        />
-        </TouchableOpacity>
-        <Header />
-        
-        <View style={styles.bodyContainer}>
-        <ImageBackground source = {require('../../../assets/Background.png') } resizeMode="cover" style = {styles.backgroundImage}>
-        
-        {props.status == 'Unlinked' ? (
-            <FancyBox style = {styles.box}>
-              <View style = {styles.alertContainer}>
-                <View style = {styles.linkIcon}>
-                  <MaterialCommunityIcons  name='link-variant' size = {20} color = {colors.white}/>
-                </View>
-                <View style = {{height: '100%', width:'95%', flexWrap:'wrap', alignItems:'center', justifyContent:'center'}}>  
-                  <Text style = {styles.alertText}>Link to your University of Alberta email to gain access to My ICard</Text>
-                
-                </View>
-              </View>
-            </FancyBox>
-          ):(<></>)}
-          {props.status == 'inactive' ? (
-            <FancyBox style = {{...styles.box}}>
-              <View style = {styles.alertContainer}>
-                <View style = {{...styles.linkIcon, backgroundColor: colors.red}}>
-                  <MaterialCommunityIcons  name='close-thick' size = {20} color = {colors.white}/>
-                </View>  
-                <View style = {{height: '100%', width:'95%', flexWrap:'wrap', alignItems:'center', justifyContent:'center'}}>
-                  <Text style = {styles.alertText}>Your account is unverified. Please go through the verification process</Text>
-                </View>
-              </View>
-            </FancyBox>
-          ) : (<></>)}
-          
-        
-        <Image
-            source={
-                require('../../../assets/squiggly-lines.png')
-            }
-            style={{...styles.lines, backgroundColor: props.status == 'active' ? 2 : 1}}
-        />
-
-        <View style={{...styles.card, backgroundColor: props.status == 'active' ? colors.primary : colors.white, borderBottomColor: statusColors[props.status], borderTopColor: statusColors[props.status], borderBottomWidth: 8, borderTopWidth: 8 }}>
-            <Image source={user != null ? {uri:user.picture } : require('../../../assets/avatar-unknown.png')} style={{...styles.avatar, borderColor: statusColors[props.status]}} />
-            <Text style={[{...styles.userName, color: props.status == 'active' ? colors.lightGreen : colors.black}, styles.subHeader]}>{user != null ? user.name : 'N/A'}</Text>
-            <View style={{...styles.statusView, backgroundColor: props.status == 'active' ? colors.primary : colors.white }}>
-            <Text style={{...styles.statusHeader, color: props.status == 'active' ? colors.lightGray : colors.black}}>ISAF Status</Text>
-            <Text style={[styles.subHeader, {...styles.ISAFStatus, color: statusColors[props.status]}]}>
-                {props.status}
-            </Text>
-            </View>
+  const {user} = props;
+  let [fontsLoaded] = useFonts({
+    Poppins_600SemiBold,
+    Poppins_700Bold,
+  });
+  if (!fontsLoaded) {
+    return <Text>Loading...</Text>;
+  }
+ 
+ 
+  const Card = ({status}) => {
+    let content
+    let color = props.status
+ 
+    if (status == 'inactive') {
+      content = (
+        <View style={styles.notification}>
+          <Image source={require('../../../assets/x.png')} style={styles.notificationPic} />
+          <View justifyContent={'center'}>
+            <Text style={styles.notificationText}>Please pay your ISAF fees to{'\n'}activate account</Text>
+          </View>
         </View>
-        {props.status == 'Unlinked' ? (
-            
-            props.children
-          
-        ): (
-            <></>
-        )}
-        {props.status == 'inactive' ? (
-          <FancyBox style = {{...styles.box, zIndex: 100, width:'60%'}}>
-            <TouchableOpacity 
-                onPress = {props.verify}
-                style={{width: '100%', padding:15, height: '100%', alignItems:'center', justifyContent:'center'}}
-                activeOpacity = {0.5} >
-              <Text style = {styles.verifyTxt}>Verify Account</Text>
-            </TouchableOpacity>
-          </FancyBox>
-        ):(<></>)}
-        {props.status != 'active' && props.status != 'inactive' ? (
-            <View style={styles.flexText}>
-            <Text style={styles.baseText}>Last Updated: 3 years ago</Text>
-            <Text style={styles.baseText}>
-                Click
-                <MaterialCommunityIcons name="refresh" size={24} color="black" />
-                for more updated info.
-            </Text>
-            </View>
-        ) : (
-            <></>
-        )}
-        <Image
-        source={require('../../../assets/ISA-logo.png')}
-        style={styles.logo}
-        />
-        </ImageBackground>
+      )}
+    else if (status == 'stale') {
+      content = (
+        <View style={styles.notification}>
+          <Image source={require('../../../assets/Refresh.png')} style={styles.notificationPic} />
+          <View justifyContent={'center'}>
+            <Text style={styles.notificationText}>Refresh page to update status</Text>
+          </View>
         </View>
-        
-         
-    </View>
-    );
-    };
+      )}
+      else if (status == 'Unlinked') {
+        content = (
+          <View style={styles.notification}>
+            <Image source={require('../../../assets/Link.png')} style={styles.notificationPic} />
+            <View justifyContent={'center'}>
+              <Text style={styles.notificationText}>Link to your University of Alberta{'\n'}email to gain access to My ICard</Text>
+            </View>
+          </View>
+        )}
+    return <View>{content}</View>
+  }
+ 
+  return (
+    <ImageBackground source={require('../../../assets/Background.png')} resizeMode="cover" style={styles.backgroundImage}>
+ 
+     
+      <Card status={props.status} />
+     
+ 
+      <View style={styles.container} backgroundColor={statusColors[props.status]} marginTop={props.status == 'inactive' || props.status == 'stale' || props.status == 'Unlinked' ? 109 : 235}>
+        <View style={styles.containerInside}>
+          <Image source={user != null ? {uri:user.picture } : require('../../../assets/account.png')} style={styles.avatar} borderColor={statusColors[props.status]} />
+          <Text style={styles.userName} >{user != null ? user.name : 'N/A'}</Text>
+          <Text style={styles.statusHeader}>ISAF status</Text>
+          <Text style={[{...styles.ISAFStatus, color: statusColors[props.status]}]} >{props.status}</Text>
+        </View>
+      </View>
+      {props.status == 'Unlinked' ? (
+           props.children
+       ): (
+           <></>
+       )}
+       
 
-   
-
-
+    </ImageBackground>
+  );
+};
+ 
 const styles = StyleSheet.create({
-  container: {
-    ...globalStyleSheet.container,
-    flex: 1,
-    width: '100%',
+  containerInside: {
+    width: 312,
+    height: 234,
+    backgroundColor: colors.white,
+    position: 'absolute',
+    marginTop: 16,
+    marginBottom: 16,
+    alignItems: 'center',
   },
-  linkIcon: {
-    borderWidth: 1,
+  container: {
+   
     borderRadius: 15,
-    padding: 5,
-    backgroundColor: colors.darkGray,
-    
+    marginTop: 235,
+    flexGrow: 1,
+    alignItems: 'center',
+    flex: 1,
+    width: 312,
+    maxHeight: 266,
+ 
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 4,
+      height: 4,
+      },
+    shadowOpacity: 0.25,
+    elevation: 5,
   },
   backgroundImage: {
-    width: '100%',
-    height: '100%',
-    justifyContent: "flex-start",
-    alignItems: "center",
-    
-    
-  },
-  bodyContainer: {
     flex: 1,
-    width: '100%',
-    ...globalStyleSheet.container,
-   
-    
-  },
-  box: {
-    marginTop: 10,
-    backgroundColor: colors.lightGray,
-    
-  },
-  alertContainer: {
-    width: '100%',
-    height: '100%',
-    flexDirection: 'row',
-    justifyContent: 'space-between',
     alignItems: 'center',
-    padding: 15
-    
-  },
-  card: {
-    ...globalStyleSheet.container,
-    flex: 0,
-    height: '50%',
-    backgroundColor: colors.white,
-    borderLeftColor: 'white',
-    borderRightColor: 'white',
-    width: '90%', 
-    borderRadius: 30,
-    paddingTop: 48,
-    elevation: 10,
-    zIndex: 1,
-    marginTop: 60,
-  },
-  signInButton: {
-    flexDirection: 'row',
-    justifyContent: 'space-around',
-    alignItems: 'center',
-    width: '60%',
-    paddingHorizontal: 24,
-    paddingVertical: 8,
-    borderWidth: 1,
-    borderRadius: 8,
-    borderColor: colors.primary,
-    marginTop: 10,
-    zIndex: 15
-  },
-  promptMessage: {
-    color: colors.primary,
   },
   avatar: {
     width: 128,
     height: 128,
     borderWidth: 3,
-    borderColor: colors.darkGray,
     borderRadius: 64,
     position: 'absolute',
-    top: -64,
-  },
-  subHeader: {
-    fontSize: 26,
-    fontFamily: 'Poppins_700Bold',
-  },
-  flexText: {
-    flex: 3,
-    paddingVertical: 24,
-    justifyContent: 'space-between',
-    alignItems: 'center',
-  },
-  verifyTxt: {
-    fontFamily: 'Poppins_600SemiBold',
-    textAlign: 'center',
-    fontSize: 16,
-    color: colors.darkGray,
-  },
-  baseText: {
-    fontFamily: 'Poppins_600SemiBold',
-    textAlign: 'center',
-    textAlignVertical: 'center',
-    fontSize: 16,
-  },
-  statusView: {
-    ...globalStyleSheet.container,
+    top: -80,
     backgroundColor: colors.white,
-    zIndex: 10,
   },
   userName: {
-    flex: 1,
-    textAlignVertical: 'center',
-    color: 'black',
-  },
-  logo: {
-    position: 'absolute',
-    right: 0,
-    bottom: 0,
-    opacity: 0.2
-  },
-  lines: {
-    position: 'absolute',
-    bottom: -64,
-    opacity: 1,
-    zIndex: 1,
-  },
-  linesTop: {
-    position: 'absolute',
-    top: -100,
-    left: -20,
-    zIndex: 1,
-  },
-  refreshButton: {
-    padding: 4,
-    backgroundColor: colors.lightGray,
-    borderWidth: 1,
-    borderColor: colors.darkGray,
-    textAlign: 'center',
-    textAlignVertical: 'center',
-    borderRadius: 36,
-    zIndex: 15,
-    position: 'absolute',
-    top: 165,
-    right: 50,
-  },
-  ISAFStatus: {
-    textTransform: 'uppercase',
-    
+    marginTop: 72,
+    fontSize: 26,
+    fontWeight: '700',
   },
   statusHeader: {
-    color: colors.black,
+    marginTop: 24,
+    fontSize: 16,
+    fontWeight: '600',
   },
-  alertText: {
-    textAlign: 'center',
-    color: colors.darkGray,
-    fontFamily: 'Poppins_600SemiBold',
+  ISAFStatus: {
+    textTransform: 'capitalize',
+    marginTop: 4,
+    fontSize: 26,
+    fontWeight: '700',
+  },
+  notification: {
+    flexDirection: 'row',
+    borderRadius: 30,
+    width: 312,
+    height: 66,
+    backgroundColor: colors.white,
+    marginTop: 60,
+    shadowOffset: {
+      width: 4,
+      height: 4,
+      },
+    shadowOpacity: 0.25,
+    elevation: 5,
+  },
+  notificationPic: {
+    width: 34,
+    height: 34,
+    marginTop: 16,
+    marginLeft: 16,
+  },
+  notificationText: {
+    marginLeft: 24,
     fontSize: 12,
-  }
+    fontWeight: '400',
+  },
 });
-
+ 
 export default MyICardPage;
+

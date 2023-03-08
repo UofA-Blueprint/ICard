97a03a621439b33b00bf7c9df2893d02b520b8e7
carlos colina <colinaperu2003@gmail.com>
2023-03-08 01:24:09 -0700

    Updating Unlinked status back

--- mobile/src/views/RegistrationView.jsx
+++ mobile/src/views/RegistrationView.jsx
@@ -59,7 +59,7 @@ const RegistrationView = () => {
 
   //status defines button condition
   return (
-    <MyICardPage user={null} status={'inactive'}>
+    <MyICardPage user={null} status={'Unlinked'}>
       <View style={styles.bodyContainer}>
         <TouchableOpacity
           disabled={!request}

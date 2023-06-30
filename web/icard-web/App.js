300ee8d1617d55f79f2510b84bb75b951ced3c92
Duc Ho <hotienduc05012002@gmail.com>
2023-06-29 23:42:39 -0600

    Added simple auth

--- web/icard-web/App.js
+++ web/icard-web/App.js
@@ -1,20 +1,15 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { GoogleOAuthProvider } from "@react-oauth/google";
+import { GoogleLogin } from "@react-oauth/google";
+import Home from "./src/views/Home";
+import secret from "./secrets/client_secrets.json";
 
 export default function App() {
+  console.log(secret);
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <GoogleOAuthProvider clientId={secret.web.client_id}>
+      <Home />
+    </GoogleOAuthProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ web/icard-web/src/views/Home.jsx
@@ -0,0 +1,71 @@
+import React, { useState, useEffect } from "react";
+import { googleLogout, useGoogleLogin } from "@react-oauth/google";
+import axios from "axios";
+import { Image, StyleSheet } from "react-native";
+
+function Home() {
+  const [user, setUser] = useState([]);
+  const [profile, setProfile] = useState([]);
+
+  const login = useGoogleLogin({
+    onSuccess: (codeResponse) => setUser(codeResponse),
+    onError: (error) => console.log("Login Failed:", error),
+  });
+
+  useEffect(() => {
+    if (user) {
+      axios
+        .get(
+          `https://www.googleapis.com/oauth2/v1/userinfo?access_token=${user.access_token}`,
+          {
+            headers: {
+              Authorization: `Bearer ${user.access_token}`,
+              Accept: "application/json",
+            },
+          }
+        )
+        .then((res) => {
+          setProfile(res.data);
+        })
+        .catch((err) => console.log(err));
+    }
+  }, [user]);
+
+  // log out function to log the user out of google and set the profile array to null
+  const logOut = () => {
+    googleLogout();
+    setProfile(null);
+  };
+  return (
+    <div>
+      <h2>React Google Login</h2>
+      {profile.name ? (
+        <div>
+          <Image
+            source={{ uri: profile.picture }}
+            alt="User Picture"
+            style={styles.image}
+          />
+          <h3>User Logged in</h3>
+          <p>Name: {profile.name}</p>
+          <p>Email Address: {profile.email}</p>
+          <button onClick={logOut}>Log out</button>
+        </div>
+      ) : (
+        <button onClick={() => login()}>
+          Sign in with Google 🚀{" "}
+        </button>
+      )}
+    </div>
+  );
+}
+
+const styles = StyleSheet.create({
+  image: {
+    width: "24px",
+    height: "24px",
+    borderWidth: 1,
+  },
+});
+
+export default Home;

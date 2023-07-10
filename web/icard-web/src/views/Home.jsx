1d4ffa5a5a03a2c1f929443fa73460a9fa64f2f0
Duc Ho <hotienduc05012002@gmail.com>
2023-07-09 21:30:56 -0600

    Added dotenv

--- web/icard-web/src/views/Home.jsx
+++ web/icard-web/src/views/Home.jsx
@@ -6,83 +6,87 @@ import {
   ImageBackground,
   TouchableOpacity,
   ScrollView,
-  SafeAreaView
-} from 'react-native';
-import DiscoverBar from '../components/home/DiscoverBar';
-import {globalStyleSheet, colors} from '../utilites/Theme';
-import VendorList from '../components/shared/VendorList'
-import {useState, useEffect, useContext} from 'react';
+  SafeAreaView,
+} from "react-native";
+import DiscoverBar from "../components/home/DiscoverBar";
+import { globalStyleSheet, colors } from "../utilites/Theme";
+import VendorList from "../components/shared/VendorList";
+import { useState, useEffect, useContext } from "react";
 //import VendorList from '../components/shared/VendorList';
 //import VendorView from './VendorView';
 //import {useState, useEffect, useContext} from 'react';
-//import {options, url} from '../data/vendorMockData';
+import { options, url } from "../data/vendorMockData2";
 //import {shuffle} from '../utilites/Shuffle';
 //import {getData} from '../data/vendorMockData';
 //import {SafeAreaView} from 'react-native-safe-area-context';
 //import {ScrollView} from 'react-native-virtualized-view';
 //import AuthContext from '../context/AuthContext';
 //import {storeUser} from '../utilites/StoreUser';
+import { API_KEY } from "@env";
+import React from "react";
+import axios from "axios";
 
-const Home = ({navigation}) => {
-  
-  const [searchPhrase, setSearchPhrase] = useState('');
+console.log(API_KEY);
+
+const Home = ({ navigation }) => {
+  const [searchPhrase, setSearchPhrase] = useState("");
   const [clicked, setClicked] = useState(false);
-  const [vendorData, setList] = useState(require('./../data/vendorMockData.json'));
-  /*
-  const {user, setUser} = useContext(AuthContext);
+  const [vendorData, setList] = useState(
+    // require("./../data/vendorMockData.json")
+    []
+  );
+
+  //const { user, setUser } = useContext(AuthContext);
 
   React.useEffect(() => {
     axios
       .get(url, options)
-      .then(res => {
+      .then((res) => {
         setList(res.data);
       })
-      .catch(error => console.log(error));
+      .catch((error) => console.log(error));
   }, []);
-  */
 
   return (
-      <ImageBackground
-      source={require('../../assets/Background.png')}
+    <ImageBackground
+      source={require("../../assets/Background.png")}
       resizeMode="cover"
-      style={styles.backgroundImage}>
-      <SafeAreaView style={{flex: 1}} edges={['top']}>
-      <ScrollView>
-      {// TODO: User == null logic and relevant functions missing
-      }
-      <TouchableOpacity>
-      <Image
-                source={require('../../assets/Sign-Out.png')}
-                style={styles.signOut}
-              />
-      </TouchableOpacity>
-      <Image
-        source={require('../../assets/ISA-logo.png')}
-        style={styles.logo}
-      />
-      <Text style={styles.title}>
-        Welcome{'\n'}to ISA's{'\n'}mobile app
-      </Text>
-      <Text style={styles.headingDiscoverDiscover}>Discover</Text>
-      <DiscoverBar />
-      <View style={styles.row}>
+      style={styles.backgroundImage}
+    >
+      <SafeAreaView style={{ flex: 1 }} edges={["top"]}>
+        <ScrollView>
+          {
+            // TODO: User == null logic and relevant functions missing
+          }
+          <TouchableOpacity>
+            <Image
+              source={require("../../assets/Sign-Out.png")}
+              style={styles.signOut}
+            />
+          </TouchableOpacity>
+          <Image
+            source={require("../../assets/ISA-logo.png")}
+            style={styles.logo}
+          />
+          <Text style={styles.title}>
+            Welcome{"\n"}to ISA's{"\n"}mobile app
+          </Text>
+          <Text style={styles.headingDiscoverDiscover}>Discover</Text>
+          <DiscoverBar />
+          <View style={styles.row}>
             <Text style={styles.headingVendor}>Vendors</Text>
 
             {/* TODO: Restore navigation functionality */}
-            <Text
-              style={styles.vendorText}
-              >
-              See All
-            </Text>
+            <Text style={styles.vendorText}>See All</Text>
           </View>
           <VendorList
             searchPhrase={searchPhrase}
             data={vendorData.slice(0, 3)}
             setClicked={setClicked}
           />
-      </ScrollView>    
+        </ScrollView>
       </SafeAreaView>
-      </ImageBackground>
+    </ImageBackground>
   );
 };
 
@@ -95,54 +99,54 @@ const styles = StyleSheet.create({
   },
   headingDiscoverDiscover: {
     fontSize: 20,
-    fontWeight: 'bold',
+    fontWeight: "bold",
     marginTop: 40,
     marginLeft: 25,
     color: colors.darkGray,
   },
   headingVendor: {
     fontSize: 20,
-    fontWeight: 'bold',
+    fontWeight: "bold",
     marginLeft: 25,
     color: colors.darkGray,
   },
   title: {
     marginLeft: 25,
     fontSize: 35,
-    fontWeight: 'bold',
+    fontWeight: "bold",
     marginTop: 11,
   },
   logo: {
     width: 100,
     height: 83,
-    resizeMode: 'stretch',
+    resizeMode: "stretch",
     marginLeft: 25,
     marginTop: 7,
   },
   backgroundImage: {
     flex: 1,
-    justifyContent: 'center',
+    justifyContent: "center",
   },
   signOut: {
     width: 24,
     height: 24,
-    resizeMode: 'stretch',
+    resizeMode: "stretch",
     marginRight: 25,
     marginTop: 24,
-    alignSelf: 'flex-end',
+    alignSelf: "flex-end",
   },
   row: {
-    flexDirection: 'row',
-    justifyContent: 'center',
-    alignItems: 'center',
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
     marginTop: 24,
-    justifyContent: 'space-between',
+    justifyContent: "space-between",
   },
   vendorText: {
     color: colors.darkGray,
     marginRight: 24,
     fontSize: 14,
-    textAlign: 'right',
+    textAlign: "right",
   },
 });
 

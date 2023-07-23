b9295695a4d1b83f4de46662745afda08da65b0f
wilsonuofa <wvtruong@ualberta.ca>
2023-07-22 18:48:50 -0600

    I think I fixed the merrge conflict

--- web/icard-web/App.js
+++ web/icard-web/App.js
@@ -1,7 +1,8 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import Home from "./src/views/Home";
+import RegistrationView from "./src/views/RegistrationView";
 
 export default function App() {
-  return <Home />;
+  return <RegistrationView />;
 }
